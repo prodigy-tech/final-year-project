ad56319ce55ad79ae0e34a946add2c95d8a8f998
prodigy-tech <96917828+prodigy-tech@users.noreply.github.com>
2023-09-29 04:21:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,11 @@ import preprocessor1
 import helper
 import matplotlib.pyplot as plt
 import pandas as pd
+
 # import seaborn as sns
 
-st.sidebar.title("ANALYZER FOR SOCIAL MEDIA PLATFORMS IMPACT ON INDIVIDUAL’S EMOTION BY SAMUEL OJO JOHN")
+st.sidebar.title("ANALYZER FOR THE IMPACT OF SOCIAL MEDIA PLATFORMS ON INDIVIDUAL’S EMOTION")
+st.sidebar.header("BY SAMUEL OJO JOHN (SCI18CSC142)")
 
 uploaded_file = st.sidebar.file_uploader("Choose a file", type=['txt'])
 if uploaded_file is not None:
@@ -17,15 +19,14 @@ if uploaded_file is not None:
 
     # # fetch unique users
     user_list = df['user'].unique().tolist()
-    user_list.remove('group_notification')
+    user_list = [user for user in user_list if user != 'group_notification']
     user_list.sort()
-    user_list.insert(0,"Overall")
+    user_list.insert(0, "Overall")
 
-    selected_user = st.sidebar.selectbox("Show analysis wrt",user_list)
+    selected_user = st.sidebar.selectbox("Show analysis wrt", user_list)
 
     if st.sidebar.button("Show Analysis"):
-
-        num_messages, r_positive, r_negative, r_neutral = helper.fetch_stats(selected_user,df)
+        num_messages, r_positive, r_negative, r_neutral = helper.fetch_stats(selected_user, df)
 
         col1, col2, col3, col4 = st.columns(4)
 
@@ -51,7 +52,7 @@ if uploaded_file is not None:
 
         positive_message = df["positive"].sum()
         negative_message = df["negative"].sum()
-        neutral_message = df["neutral"].sum()-df[df['message'] == '<Media omitted>\n'].shape[0]
+        neutral_message = df["neutral"].sum() - df[df['message'] == '<Media omitted>\n'].shape[0]
         col1, col2 = st.columns(2)
 
         with col1:
@@ -71,3 +72,12 @@ if uploaded_file is not None:
             plt.pie(sizes, labels=labels, colors=colors, autopct="%1.1f%%", startangle=140)
             plt.axis("equal")  # Equal aspect ratio ensures the pie chart is circular.
             st.pyplot()
+
+    df_wc = helper.create_wordcloud(selected_user, df)
+    if df_wc is not None:
+        # Display the word cloud using Matplotlib
+        fig, ax = plt.subplots()
+        ax.imshow(df_wc)
+        st.pyplot(fig)
+    else:
+        st.write("No messages available for word cloud. Kindly change your time format to 24hrs before exporting uploaded .txt file")
