de9579faa48a283353933a608b2993a1bd2af620
prodigy-tech <96917828+prodigy-tech@users.noreply.github.com>
2023-09-29 04:22:57 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,5 +1,6 @@
-import re
-import matplotlib.pyplot as plt
+from urlextract import URLExtract
+extract = URLExtract
+from wordcloud import WordCloud
 
 def fetch_stats(selected_user,df):
 
@@ -28,3 +29,22 @@ def media(df):
         words.extend(message.split())
     num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
     return num_media_messages
+
+
+def create_wordcloud(selected_user, df):
+    if selected_user == "Overall":
+        filtered_df = df
+    else:
+        filtered_df = df[df['user'] == selected_user]
+
+    # Convert non-string messages to string
+    filtered_df['message'] = filtered_df['message'].astype(str)
+
+    # Generate word cloud
+    messages = filtered_df['message'].str.cat(sep=" ")
+
+    if messages.strip():  # Check if there are messages to generate a word cloud
+        wc = WordCloud(width=800, height=400, background_color='white').generate(messages)
+        return wc
+    else:
+        return None
